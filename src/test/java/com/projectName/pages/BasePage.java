28090fc9a10e762cdea277a0bae739f55c052365
svetko <ssadmitrovic@gmail.com>
2024-04-17 12:43:22 -0400

    master branch - some files deleted

--- src/test/java/com/projectName/pages/BasePage.java
+++ src/test/java/com/projectName/pages/BasePage.java
@@ -1,26 +1,16 @@
 package com.projectName.pages;
 
 import com.projectName.utilities.Driver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+
 public abstract class BasePage {
+
 
     private BasePage() {
         PageFactory.initElements(Driver.getDriver(),this);
 
     }
-
-
-    @FindBy(css = ".login-link")
-    public WebElement login;
-
-    @FindBy(linkText = "My Account")
-    public WebElement myAccount;
-
-
-    
 
 
 }
